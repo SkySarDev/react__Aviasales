59be4f12eb2626c8597c11a1b5229e0d7510e66e
SkySarDev <skysar@ya.ru>
2021-08-23 21:38:48 +0300

    Feat: styling component SortTickets

--- src/components/SortTickets/SortTickets.jsx
+++ src/components/SortTickets/SortTickets.jsx
@@ -1,16 +1,30 @@
-import React from "react";
-import classes from "./SortTickets.module.css";
-import Block from "../../UI/Block";
+import React, { useState } from "react";
+import classes from "./SortTickets.module.scss";
 
 const SortTickets = () => {
+  const [active, setActive] = useState("cheaper");
+
+  const changeActive = (e) => setActive(e.target.dataset.key);
+
+  const renderSortButton = (text, name, classname) => {
+    return (
+      <li
+        className={`${classes[classname]}
+          ${active === name ? classes.active : ""}`}
+        data-key={name}
+        onClick={changeActive}
+      >
+        {text}
+      </li>
+    );
+  };
+
   return (
-    <Block>
-      <div className={classes.wrapper}>
-        <div className={classes.filterElement}>Самый дешевый</div>
-        <div className={classes.filterElement}>Самый быстрый</div>
-        <div className={classes.filterElement}>Оптимальный</div>
-      </div>
-    </Block>
+    <ul className={classes.sortingPanel}>
+      {renderSortButton("Самый дешевый", "cheaper", "left")}
+      {renderSortButton("Самый быстрый", "faster", "center")}
+      {renderSortButton("Оптимальный", "optimal", "right")}
+    </ul>
   );
 };
 

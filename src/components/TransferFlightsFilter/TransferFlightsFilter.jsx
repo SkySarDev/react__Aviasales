c04f2457cd2868d1d373744b037ed716653e3cb8
SkySarDev <skysar@ya.ru>
2021-08-24 17:29:14 +0300

    Feat: styling component TransferFlightsFilter

--- src/components/TransferFlightsFilter/TransferFlightsFilter.jsx
+++ src/components/TransferFlightsFilter/TransferFlightsFilter.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import classes from "./TransferFlightsFilter.module.css";
+import classes from "./TransferFlightsFilter.module.scss";
 import Block from "../../UI/Block";
 
 const TransferFlightsFilter = () => {
+  const filterList = [
+    "Все",
+    "Без пересадок",
+    "1 пересадка",
+    "2 пересадки",
+    "3 пересадки",
+  ];
+
   return (
     <Block>
-      <h3 className={classes.title}>Количество пересадок</h3>
+      <div>
+        <h3 className={classes.title}>Количество пересадок</h3>
+        <div className={classes.filterList}>
+          {filterList.map((filterName, i) => {
+            return (
+              <div className={classes.filerItem} key={filterName}>
+                <input type="checkbox" id={`filter-checkbox-${i}`} />
+                <label htmlFor={`filter-checkbox-${i}`}>{filterName}</label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Block>
   );
 };

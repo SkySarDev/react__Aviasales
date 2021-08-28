516d61f4e9bafddc1bd44d7e07bc0fd5d2df2be7
SkySarDev <skysar@ya.ru>
2021-08-28 19:01:21 +0300

    Feat: add animations w/ framer-motion

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,49 +1,48 @@
 import React, { useContext } from "react";
 import classes from "./MainContent.module.scss";
-import animations from "../../UI/Animations.module.scss";
 import TransferFlightsFilter from "../TransferFlightsFilter/TransferFlightsFilter";
 import SortTickets from "../SortTickets/SortTickets";
 import Button from "../../UI/Button";
-import { Transition } from "react-transition-group";
 import TicketList from "../Tickets/TicketList";
 import { TicketsContext } from "../../state/TicketsContext";
+import { motion, AnimatePresence } from "framer-motion";
 
 const MainContent = ({ onSearchTickets }) => {
   const { state, showLoading, getData } = useContext(TicketsContext);
+  const showContent = !!state.ticketStack.length;
+  const animations = {
+    button: {
+      animate: { opacity: 1 },
+      exit: { opacity: 0 },
+      transition: { delay: 0.2, duration: 0.3 },
+    },
+    content: {
+      initial: { opacity: 0, y: 50 },
+      animate: {
+        opacity: 1,
+        y: 0,
+        transition: { type: "spring", delay: 0.55 },
+      },
+    },
+  };
 
   return (
     <div className={classes.mainWrapper}>
-      <Transition
-        in={!state.ticketStack.length}
-        timeout={{
-          exit: 300,
-        }}
-        unmountOnExit
-      >
-        {(animationState) => (
-          <div
-            className={`${classes.searchField} ${animations[animationState]}`}
-          >
+      <AnimatePresence initial={false}>
+        {!showContent && (
+          <motion.div className={classes.searchField} {...animations.button}>
             <Button
               name={"Найти билеты"}
               onHandleClick={getData}
               showLoading={showLoading}
             />
-          </div>
+          </motion.div>
         )}
-      </Transition>
+      </AnimatePresence>
 
-      <Transition
-        in={!!state.ticketStack.length}
-        timeout={{
-          enter: 300,
-        }}
-        mountOnEnter
-      >
-        {(animationState) => (
-          <div
-            className={`${classes.ticketsField} ${animations[animationState]}`}
-          >
+      <AnimatePresence>
+        {showContent && (
+          <motion.div className={classes.ticketsField} {...animations.content}>
             <div className={classes.sideBar}>
               <TransferFlightsFilter />
             </div>
@@ -58,9 +57,9 @@ const MainContent = ({ onSearchTickets }) => {
                 />
               </div>
             </div>
-          </div>
+          </motion.div>
         )}
-      </Transition>
+      </AnimatePresence>
     </div>
   );
 };
--- src/components/Tickets/TicketList.jsx
+++ src/components/Tickets/TicketList.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import classes from "./TicketList.module.scss";
-import animations from "../../UI/Animations.module.scss";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { motion, AnimatePresence } from "framer-motion";
 import TicketItem from "./TicketItem";
 
 const TicketList = ({ tickets }) => {
+  const animations = {
+    initial: {
+      opacity: 0,
+      y: 25,
+    },
+    animate: (i) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.05,
+        type: "spring",
+      },
+    }),
+  };
+
   return (
-    <ul className={classes.ticketList}>
-      <TransitionGroup>
+    <AnimatePresence exitBeforeEnter>
+      <ul className={classes.ticketList}>
         {tickets.map((ticket, i) => (
-          <CSSTransition
-            key={ticket.price + i}
-            timeout={500}
-            classNames={animations}
-            appear={true}
-          >
-            <li>
-              <TicketItem ticketRowData={ticket} />
-            </li>
-          </CSSTransition>
+          <motion.li key={ticket.price + i} {...animations} custom={i}>
+            <TicketItem ticketRowData={ticket} />
+          </motion.li>
         ))}
-      </TransitionGroup>
-    </ul>
+      </ul>
+    </AnimatePresence>
   );
 };
 
